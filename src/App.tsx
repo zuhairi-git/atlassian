3172927a2fb5cc488431fef48a42d90ddf718aa9
Ali Al-Zuhairi <zohairi@live.com>
2023-01-16 22:25:34 +0200

    Add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AtlassianNavigation } from '@atlaskit/atlassian-navigation';
+import AtlassianNav from './pages/AtlassianNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <AtlassianNav />
+    </>
+    
+    );
 }
 
 export default App;
