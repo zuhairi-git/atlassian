8b023523e9da7144ce7a01c7b2c783ffde4f20d4
Ali Al-Zuhairi <zohairi@live.com>
2023-07-12 07:26:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { AtlassianNavigation } from '@atlaskit/atlassian-navigation';
 import AtlassianNav from './pages/AtlassianNav';
 
 function App() {
   return (
     <>
-    <AtlassianNav />
+      <AtlassianNav />
     </>
-    
-    );
+
+  );
 }
 
 export default App;
